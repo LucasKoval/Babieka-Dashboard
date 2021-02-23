bbb6be7c79ba776180d7e5a5c1aca763a0f2b6fd
fedegox <fedegox775@gmail.com>
2021-02-22 21:25:00 -0300

    listUsers

--- src/assets/UserUnit.js
+++ src/assets/UserUnit.js
@@ -4,9 +4,12 @@ import propTypes from 'prop-types';
 function Category(props) {
     return (
         <div className="col-lg-6 mb-4">
-            <div className="card bg-danger text-white shadow">
+            <div className="justify-content-space-between card bg-danger text-white shadow">
                 <div className="card-body">
                     {props.name}
+                    <span className="card-body">
+                        {props.role}
+                    </span>
                 </div>
             </div>
         </div> 
--- src/pages/main/content/main/pageContent/ContentRow2.js
+++ src/pages/main/content/main/pageContent/ContentRow2.js
@@ -2,14 +2,18 @@ import React from 'react';
 import propTypes from 'prop-types';
 import LastProduct from './contentRow2/LastProduct'
 import Categories from './contentRow2/Categories'
+import UserList from './contentRow2/UsersList'
 
 function ContentRow2() {
     return (
         <div className="row">
             <LastProduct />
             <Categories />
+            <UserList />
         </div>
     );
 }
+
+    
 
 export default ContentRow2;
