02d1222df1cc9fa93d0793394c06939365410af6
fedegox <fedegox775@gmail.com>
2021-03-01 16:39:17 -0300

    list

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PageHeading from '../../assets/PageHeading'
+import UsersList from '../../components/blockSection/assets/UsersList';
+import Box from '../../components/box/Box'
+
 
 
 function Users() {
+    
     return (
         <div id="content">
             <div className="container-fluid">
                 <PageHeading title='BABIEKA - Users'/>
+                <UsersList/>
+                <Box title='Listado de usuarios'/>
+
+
                 
             </div>
         </div>
