2407fde9fee7554a402de0204d1648bd158cab35
LucasKoval <lucaskoval@hotmail.com>
2021-03-17 20:12:14 -0300

    APIs ready to deploy

--- src/apis/baseUrl.js
+++ src/apis/baseUrl.js
@@ -1,14 +1,14 @@
 // API Products
-export const BASE_API_PRODUCTS_URL = "http://localhost:3030/api/products/";
-export const BASE_API_PRODUCTS_LIST_URL = "http://localhost:3030/api/products/list";
-export const BASE_API_MODELS_URL = "http://localhost:3030/api/products/models";
-export const BASE_API_MODELS_LIST_URL = "http://localhost:3030/api/products/models/list";
+export const BASE_API_PRODUCTS_URL = "https://babieka.herokuapp.com/api/products/";
+export const BASE_API_PRODUCTS_LIST_URL = "https://babieka.herokuapp.com/api/products/list";
+export const BASE_API_MODELS_URL = "https://babieka.herokuapp.com/api/products/models";
+export const BASE_API_MODELS_LIST_URL = "https://babieka.herokuapp.com/api/products/models/list";
 
 // API Users
-export const BASE_API_USERS_URL = "http://localhost:3030/api/users/";
-export const BASE_API_USERS_LIST_URL = "http://localhost:3030/api/users/list";
+export const BASE_API_USERS_URL = "https://babieka.herokuapp.com/api/users/";
+export const BASE_API_USERS_LIST_URL = "https://babieka.herokuapp.com/api/users/list";
 
 // API Cart
-export const BASE_API_ORDERS_URL = "http://localhost:3030/api/cart/orders";
-export const BASE_API_ITEMS_URL = "http://localhost:3030/api/cart/items";
-export const BASE_API_LAST_ADDED_URL = "http://localhost:3030/api/cart/lastAdded";
+export const BASE_API_ORDERS_URL = "https://babieka.herokuapp.com/api/cart/orders";
+export const BASE_API_ITEMS_URL = "https://babieka.herokuapp.com/api/cart/items";
+export const BASE_API_LAST_ADDED_URL = "https://babieka.herokuapp.com/api/cart/lastAdded";
